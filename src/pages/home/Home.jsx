2087c8b8bae5ee70962a12e73a85bb61edc3c17e
PattyCutie <iamsureiamcute@gmail.com>
2022-07-19 21:10:51 +0200

    Last transection table section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import Sidebar from '../../components/sidebar/Sidebar'
 import Widget from '../../components/widget/Widget'
 import Featured from '../../components/featured/Featured'
 import Chart from '../../components/chart/Chart'
+import Table from '../../components/table/Table'
 import "./home.scss"
 
 function Home() {
@@ -21,6 +22,10 @@ function Home() {
           <div className="charts">
              <Featured />
              <Chart />
+          </div>
+          <div className="listContainer">
+            <div className="listTitle">Latest Transections</div>
+            <Table />
           </div>
         </div>
     </div>
